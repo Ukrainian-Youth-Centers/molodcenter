6dddbdc6fccc1ccd6a606d3cb21557ea8c4caf68
Katok777-foravailable <svatoslav22882288@gmail.com>
2026-08-16 13:16:39 +0300

    пофікшений баг

--- src/main/java/com/katok/molodcenter/category/CategoryCreateDto.java
+++ src/main/java/com/katok/molodcenter/category/CategoryCreateDto.java
@@ -1,7 +1,6 @@
 package com.katok.molodcenter.category;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,6 +13,5 @@ import lombok.NoArgsConstructor;
 public class CategoryCreateDto {
     @NotBlank
     private String name;
-    @NotNull
     private Long youthCenterId;
 }
